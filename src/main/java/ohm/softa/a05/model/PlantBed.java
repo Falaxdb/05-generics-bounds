463769d65f46ebcc77efae1d77868fc7fa97ea12
Falaxdb <Falaxdb@proton.me>
2024-01-15 00:21:02 +0100

    Implement Plantbed

--- src/main/java/ohm/softa/a05/model/PlantBed.java
+++ src/main/java/ohm/softa/a05/model/PlantBed.java
@@ -1,4 +1,27 @@
 package ohm.softa.a05.model;
 
-public class PlantBed {
+import ohm.softa.a05.collections.SimpleFilter;
+import ohm.softa.a05.collections.SimpleList;
+import ohm.softa.a05.collections.SimpleListImpl;
+
+public class PlantBed<T extends Plant> {
+
+    SimpleList<T> plants;
+
+    public PlantBed(){
+        this.plants = new SimpleListImpl<T>();
+    }
+
+    public void add (T plant){
+        plants.add(plant);
+    }
+
+    public int size (){
+        return plants.size();
+    }
+
+    public SimpleList<T> getPlantsByColor(PlantColor color){
+        return plants.filter(o -> (o.getColor() == color) );
+    }
+
 }
